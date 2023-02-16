4c39521b3f98dda64e8a229cfa4acfb6e3d04709
alieyup <alieyup18@gmail.com>
2023-02-16 09:14:37 +0300

    Class'ların Oluşturulması.

---
+++ EntityLayer/Concrete/Blog.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Blog
+    {
+        //Erişim Belirleyici Türü - Değişken Türü - İsim - {get set}
+
+        [Key]
+        public int BlogID { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogContent { get; set; }
+        public string BlogThumbnailImage { get; set; }
+        public string BlogImage { get; set; }
+        public DateTime BlogCreateDate { get; set; }
+        public bool BlogStatus { get; set; }
+    }
+}
