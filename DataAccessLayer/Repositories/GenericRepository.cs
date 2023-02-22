88441a475c15ad713d188263e8c7044d7ac78cc4
alieyup <alieyup18@gmail.com>
2023-02-22 08:43:09 +0300

    GenericRepository Oluşturulması

--- DataAccessLayer/Repositories/GenericRepository.cs
+++ DataAccessLayer/Repositories/GenericRepository.cs
@@ -1,4 +1,5 @@
 ﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +8,38 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Repositories
 {
-    public class GenericRepository<T>:IGenericDal where T : class
+    public class GenericRepository<T> : IGenericDal<T> where T : class
     {
+        public void Add(T t)
+        {
+            using var c = new Context();
+            c.Add(t);
+            c.SaveChanges();
+        }
 
+        public void Remove(T t)
+        {
+            using var c = new Context();
+            c.Remove(t);
+            c.SaveChanges();
+        }
+
+        public void Update(T t)
+        {
+            using var c = new Context();
+            c.Update(t);
+            c.SaveChanges();
+        }
+        public T GetById(int id)
+        {
+            using var c = new Context();
+            return c.Set<T>().Find(id);
+        }
+
+        public List<T> GetListAll()
+        {
+            using var c = new Context();
+            return c.Set<T>().ToList();
+        }
     }
 }
