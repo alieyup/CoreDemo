a9d07c615a128e0d97ceab6c28145999117411d8
alieyup <alieyup18@gmail.com>
2023-02-27 11:31:14 +0300

    Writer ekleme bölümü Veritabanını httppost metodu ile veri ekleme 34. dersin sonu

---
+++ CoreDemo/Controllers/RegisterController.cs
@@ -0,0 +1,37 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFreamwork;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+	public class RegisterController : Controller
+	{
+		WriterManager wm = new WriterManager(new EfWriterRepository());
+
+
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		/*Ekleme işlemi yapılırken HttpPost ve HttpGet attributelerin tanımlandığı metodların isimleri aynı olmak zorundadır. 
+		HttpGet - Sayfa yüklenince
+		HttpPost - Sayfada Buton tetiklenince
+
+		Mesela HttpGet Attribute komutu sayfada kategorize veya benzeri işlemler kullanılırken sayfa yüklendiği anda listelenen niteliklerde kullanılabilir 
+		Örn: İller / İlçeler HttpGet Attribute içiresine yazılabilir.
+		     Kategoriler/AltKategoriler HttpGet Attribute içiresine yazılabilir.
+		*/
+
+		[HttpPost]
+		public IActionResult Index(Writer p)
+		{
+			p.WriterStatus = true;
+			p.WriterAbout = "Deneme Test";
+			wm.WriterAdd(p);
+			return RedirectToAction("Index", "Blog");
+		}
+	}
+}
