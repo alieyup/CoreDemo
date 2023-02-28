d8b29efe6c46c9b4de99f7e2bd8c938f6460871e
alieyup <alieyup18@gmail.com>
2023-02-28 10:00:16 +0300

    Yazar ve Blog İlişkilendirmesi, Yazara ait son yazıların çekilmesi (statik brakıldı.) 37. dersin sonu.

--- CoreDemo/ViewComponents/Blog/WriterLastBlog.cs
+++ CoreDemo/ViewComponents/Blog/WriterLastBlog.cs
@@ -1,6 +1,17 @@
-﻿namespace CoreDemo.ViewComponents.Blog
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFreamwork;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents.Blog
 {
-	public class WriterLastBlog
+	public class WriterLastBlog : ViewComponent
 	{
+		BlogManager bm = new BlogManager(new EfBlogRepository());
+
+		public IViewComponentResult Invoke()
+		{
+			var values = bm.GetBlogListByWriter(1);
+			return View(values);
+		}
 	}
 }
