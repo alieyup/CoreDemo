335b0ef416da3aef52bf96afceb69aa76637bc80
alieyup <alieyup18@gmail.com>
2023-02-28 12:24:19 +0300

    NewsLetter tüm mimari baştan oluşturuldu. BusinessLayer - DataAccessLayer ve EntityLayer DB Tablosu eklendi migrate ve db update yapıldı. 38. dersin sonu.

---
+++ CoreDemo/Controllers/NewsLetterController.cs
@@ -0,0 +1,24 @@
+﻿using DataAccessLayer.EntityFreamwork;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using BusinessLayer.Concrete;
+
+namespace CoreDemo.Controllers
+{
+	public class NewsLetterController : Controller
+	{
+		NewsLetterManager nm = new NewsLetterManager(new EfNewsLetterRepository());
+		[HttpGet]
+		public PartialViewResult SubscribeMail()
+		{
+			return PartialView();
+		}
+		[HttpPost]
+		public PartialViewResult SubscribeMail(NewsLetter p)
+		{
+			p.MailStatus = true;
+			nm.AddNewsLetter(p);
+			return PartialView();
+		}
+	}
+}
