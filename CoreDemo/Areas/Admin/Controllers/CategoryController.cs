12c67d1e486f5d2a41f06e47a174ae93d7183573
alieyup <alieyup18@gmail.com>
2023-04-12 10:24:29 +0300

    Confirm uyarı ile işlem yapma kodu yazıldı. 94. dersin sonu.

--- CoreDemo/Areas/Admin/Controllers/CategoryController.cs
+++ CoreDemo/Areas/Admin/Controllers/CategoryController.cs
@@ -1,13 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFreamwork;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using X.PagedList;
 
 namespace CoreDemo.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
+        public IActionResult Index(int page =1)
+        {
+            var values = cm.GetList().ToPagedList(page, 3);
+            //var values = cm.GetList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddCategory()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult AddCategory(Category p) 
+        {
+            CategoryValidator cv = new CategoryValidator();
+            ValidationResult results = cv.Validate(p);
+        
+            if (results.IsValid)
+            {
+                p.CategoryStatus = true;
+                cm.TAdd(p);
+                return RedirectToAction("Index", "Category");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
+        public IActionResult DeleteCategory(int id)
+        {
+            var catvalue = cm.TGetById(id);
+            cm.TRemove(catvalue);
+            return RedirectToAction("Index");
         }
     }
 }
