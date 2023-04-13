622a26de33abb76aedd20bcffeedd1ff32211066
alieyup <alieyup18@gmail.com>
2023-04-13 16:40:11 +0300

    Widgets, Admin paneli istatistiklerin çekilmesi 100. dersin sonu

---
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFreamwork;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic1 : ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        Context context = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.BlogCount = bm.GetList().Count();
+            ViewBag.MessageCount = context.Message2s.Count();
+            ViewBag.CommentCount = context.Comments.Count();
+            return View();
+        }
+    }
+}
---
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic2.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
+
+namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic2 : ViewComponent
+    {
+        Context context = new Context();
+        public IViewComponentResult Invoke()
+        {
+            var blogid = context.Blogs.OrderByDescending(x => x.BlogID).Select(x => x.WriterID).Take(1).FirstOrDefault();
+            ViewBag.SonEklenenBlog = context.Blogs.OrderByDescending(x => x.BlogTitle).Select(x=>x.BlogTitle).Take(1).FirstOrDefault();
+            ViewBag.SonBlogYazari = context.Writers.Where(x => x.WriterID == blogid).Select(x => x.WriterName).FirstOrDefault();
+            return View();
+        }
+    }
+}
