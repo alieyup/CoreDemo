d68603a1ac238412fb88fbf4e5c9d736bd538d8c
alieyup <alieyup18@gmail.com>
2023-04-14 16:44:51 +0300

    Admin tablosu eklendi, widget sayfasında istatistik bilgisi çekildi. 102. dersin sonu.

---
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic4.cs
@@ -0,0 +1,18 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic4 : ViewComponent
+    {
+        Context context = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.AdminIsim = context.Admins.Where(x => x.AdminID == 2).Select(x => x.Name).FirstOrDefault();
+            ViewBag.ShortDesc = context.Admins.Where(x => x.AdminID == 2).Select(x => x.ShortDescription).FirstOrDefault();
+            ViewBag.Image = context.Admins.Where(x => x.AdminID == 2).Select(x => x.ImageURL).FirstOrDefault();
+
+            return View();
+        }
+    }
+}
--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -40,7 +40,8 @@ namespace DataAccessLayer.Concrete
         public DbSet<Notification> Notifications { get; set; }
         public DbSet<Message> Messages { get; set; }
 		public DbSet<Message2> Message2s { get; set; }
+        public DbSet<Admin> Admins { get; set; }
 
 
-	}
+    }
 }
