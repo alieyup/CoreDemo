a015afa0a097a35dc4b2322296300c0c9298e4c7
alieyup <alieyup18@gmail.com>
2023-04-17 14:08:41 +0300

    Google charts kullanımı 105. dersin sonu.

---
+++ CoreDemo/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,35 @@
+﻿using CoreDemo.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryChart()
+        {
+            List<CategoryClas> list = new List<CategoryClas>();
+            list.Add(new CategoryClas 
+            { 
+                categoryname = "Yazılım",  
+                categorycount = 10 
+            });
+            list.Add(new CategoryClas
+            {
+                categoryname = "Teknoloji",
+                categorycount = 7
+            });
+            list.Add(new CategoryClas
+            {
+                categoryname = "Spor",
+                categorycount = 19
+            });
+            return Json(new { jsonlist = list });
+        }
+    }
+}
