5b53ff05b189901138a52e31897a8761646489fc
alieyup <alieyup18@gmail.com>
2023-04-17 12:49:59 +0300

    Xml Api ile hava durumu çekildi. 103. dersin sonu.

--- CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -2,6 +2,7 @@
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFreamwork;
 using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 
 namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
 {
@@ -14,6 +15,13 @@ namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
             ViewBag.BlogCount = bm.GetList().Count();
             ViewBag.MessageCount = context.Message2s.Count();
             ViewBag.CommentCount = context.Comments.Count();
+
+            string api = "3787e8ae13aaa593347e31a4e45380b4";
+            string sehir = "Ankara";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=" + sehir + ",tr&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.sicaklik = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+            ViewBag.sehir = sehir;
             return View();
         }
     }
