b371515151c7c5cef1326633141a5953e6e670c8
alieyup <alieyup18@gmail.com>
2023-04-19 14:13:53 +0300

    BlogAPI class ı eklendi Yeni db ve tablo eklendi swagger ve api getlist postman ile istek 116. dersin sonu

---
+++ BlogApiDemo/DataAccessLayer/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlogApiDemo.DataAccessLayer
+{
+	public class Context:DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("server=AB60013-0570;database=CoreBlogApiDb;integrated security=true;TrustServerCertificate=True");
+			//  optionsBuilder.UseSqlServer("server=ALI\\SQLEXPRESS;database=CoreBlogDb;integrated security=true;TrustServerCertificate=True");
+		}
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
