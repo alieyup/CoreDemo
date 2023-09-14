dcc46bd9605e1c65f9791dd5858de613ded3497b
AB129050 <AB129050@AB60013-129050>
2023-09-14 08:39:00 +0300

    120. ders patlamadan önceki hali

---
+++ CoreDemo/Controllers/EmployeeTestController1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CoreDemo.Controllers
+{
+    public class EmployeeTestController1 : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var httpClient = new HttpClient();
+            var responseMessage = await httpClient.GetAsync("");
+            var jsonString=await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<Class1>>(jsonString);
+            return View(values);
+        }
+        public class Class1 {
+            public int ID { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
