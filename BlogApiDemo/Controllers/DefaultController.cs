e4fd6212cf3cc4fc741384c1b9c656a88221fe70
AB129050 <AB129050@AB60013-129050>
2023-09-14 11:56:30 +0300

    14/09 öğle arası güncellemesi

--- BlogApiDemo/Controllers/DefaultController.cs
+++ BlogApiDemo/Controllers/DefaultController.cs
@@ -38,5 +38,32 @@ namespace BlogApiDemo.Controllers
 			}
 
 		}
-	}
+        [HttpDelete("{id}")]
+        public IActionResult EmployeeDelete(int id)
+        {
+            using var c = new Context();
+            var employee = c.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            c.Remove(employee);
+            c.SaveChanges();
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult EmployeeUpdate(Employee employee)
+        {
+            using var c = new Context();
+            var emp = c.Employees.Find(employee.ID);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            emp.Name = employee.Name;
+            c.Update(emp);
+            c.SaveChanges();
+            return Ok();
+        }
+    }
 }
